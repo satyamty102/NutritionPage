78f604a14138b428c37ab172e8c4308c6600cfe6
ALOK SINGH <14109008alok@gmail.com>
2022-01-19 15:12:31 +0530

    Nutrition page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Center1 from './components/center1';
+
+function App() {
+  return (
+      <>
+      <Center1/>
+      </>
+  );
+}
+
+export default App;
